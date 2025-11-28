62394ca1bc0f3f5eb77f9abab218aabe3292bd4f
Paul ALNET <alnet@insa-toulouse.fr>
2025-11-28 16:51:30 +0100

    refactor: release usage of TIM2

--- Application/principal.c
+++ Application/principal.c
@@ -8,11 +8,11 @@
 
 /////////////////////////////////
 //  fonction 'pas top top'     //
-//  d'attente d'environ 250 ms //
+//  d'attente d'environ 50 ms  //
 /////////////////////////////////
 void Attente (void)
 {
-	for (int i = 0 ; i < 1000000 ; i++){}
+	for (int i = 0 ; i < 200000 ; i++){}
 }
 /////////////////////////////////
 int main (void) {
@@ -38,25 +38,11 @@ int main (void) {
 	RCC->APB1ENR |= RCC_APB2ENR_SPI1EN;
 	Roulis_init(SPI1);
 
-	// Enable clock for timer TIM2, 72 MHz
-	// 500ms is 36 000 000 clock cycles.
-	MyTimer_Base_Init(TIM2, 360, 10000);
-	MyTimer_Base_Start(TIM2);
-	
-	// Configure interrupt
-	MyTimer_ActiveIT(TIM2, 2);
-	while (1) {};
-}
-
-// called on TIM2 interruptions
-void TIM2_IRQHandler ( void )
-{
-	// Reset update activation bit, should have been set by peripheral on event
-	TIM2->SR &= ~TIM_SR_UIF;
-
-	
-	if (Roulis_isBoatInBadShape()) 
-		Ecoute_setTheta(0);
-	else
-		Ecoute_setTheta(AlphaToTheta(Girouette_GetAlpha()));
+	while (1) {
+		Attente();
+		if (Roulis_isBoatInBadShape())
+			Ecoute_setTheta(0);
+		else
+			Ecoute_setTheta(AlphaToTheta(Girouette_GetAlpha()));
+	};
 }
